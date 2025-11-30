c005487abcdd260e23b4adeefcecf22d67dba284
Oliver Forral <intrepion@gmail.com>
2025-11-30 08:24:33 -0800

    Query parameter list / multiple values

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def read_file(file_path: str):
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(min_length=3)]):
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
