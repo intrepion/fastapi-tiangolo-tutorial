370cf91e7bde57ac79e640507a608927f4836983
Oliver Forral <intrepion@gmail.com>
2025-11-30 08:23:31 -0800

    Required, can be None

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def read_file(file_path: str):
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str, Query(min_length=3)]):
+async def read_items(q: Annotated[str | None, Query(min_length=3)]):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
