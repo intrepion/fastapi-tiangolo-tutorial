903956f6f90537aa8d0c9d76140b66858c88f8a6
Oliver Forral <intrepion@gmail.com>
2025-11-30 08:32:27 -0800

    Alias parameters

--- main.py
+++ main.py
@@ -36,16 +36,7 @@ async def read_file(file_path: str):
 
 
 @app.get("/items/")
-async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-        ),
-    ] = None,
-):
+async def read_items(q: Annotated[str | None, Query(alias="item-query")] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
