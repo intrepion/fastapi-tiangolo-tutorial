24b5dbee8551257890429d8e35bacdbd31b95055
Oliver Forral <intrepion@gmail.com>
2025-11-30 08:20:29 -0800

    Add regular expressions

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ async def read_file(file_path: str):
 
 @app.get("/items/")
 async def read_items(
-    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
