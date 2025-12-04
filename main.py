9ca11c853bd3be3a3ef147421f4d985fb5fe8c75
Oliver Forral <intrepion@gmail.com>
2025-12-04 11:54:26 -0800

    List fields

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class Item(BaseModel):
     )
     price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
+    tags: list = []
 
 
 class ModelName(str, Enum):
