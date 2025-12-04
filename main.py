1b18844c5a1c421cb7b1ee41f5df1305ac2cedb7
Oliver Forral <intrepion@gmail.com>
2025-12-04 11:51:33 -0800

    Import Field

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ class FilterParams(BaseModel):
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
 
