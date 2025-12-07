5c07c7bdd512ae26148b208549bdaaba076a77af
Oliver Forral <intrepion@gmail.com>
2025-12-07 02:02:29 -0800

    UploadFile with Additional Metadata

--- main.py
+++ main.py
@@ -168,11 +168,8 @@ async def root():
 
 
 @app.post("/files/")
-async def create_file(file: Annotated[bytes | None, File()] = None):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        return {"file_size": len(file)}
+async def create_file(file: Annotated[bytes, File(description="A file read as bytes")]):
+    return {"file_size": len(file)}
 
 
 @app.get("/files/{file_path:path}")
@@ -282,11 +279,10 @@ async def get_teleport() -> RedirectResponse:
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile | None = None):
-    if not file:
-        return {"message": "No upload file sent"}
-    else:
-        return {"filename": file.filename}
+async def create_upload_file(
+    file: Annotated[UploadFile, File(description="A file read as UploadFile")],
+):
+    return {"filename": file.filename}
 
 
 @app.post("/user/", response_model=UserOut)
