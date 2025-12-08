e488544d99f3327cf97b5ff354ed6808e5acb05d
Oliver Forral <intrepion@gmail.com>
2025-12-08 13:53:23 -0800

    OAuth2PasswordRequestForm

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from fastapi.responses import (
     PlainTextResponse,
     RedirectResponse,
 )
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import AfterValidator, BaseModel, EmailStr, Field, HttpUrl
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from typing import Annotated, Any, List, Literal, Union
@@ -163,7 +163,7 @@ class UserOut(UserBase):
     pass
 
 
-class UserInDB(UserBase):
+class UserInDB(User):
     hashed_password: str
 
 
@@ -175,6 +175,23 @@ data = {
 fake_db = {}
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 items = {
     "foo": {"name": "Foo", "price": 50.2},
@@ -197,13 +214,30 @@ async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 10
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
 
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
 def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 
 def fake_password_hasher(raw_password: str):
@@ -256,6 +290,14 @@ async def verify_token(x_token: Annotated[str, Header()]):
 CommonsDep = Annotated[dict, Depends(common_parameters)]
 
 app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 
 @app.exception_handler(StarletteHTTPException)
@@ -442,6 +484,19 @@ async def get_teleport() -> RedirectResponse:
     return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
 @app.get("/unicorns/{name}")
 async def read_unicorn(name: str):
     if name == "yolo":
@@ -476,11 +531,6 @@ async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
 
-@app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
-    return current_user
-
-
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
@@ -498,3 +548,10 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
