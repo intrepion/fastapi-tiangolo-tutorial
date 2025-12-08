9d3bb38d07681e4a184f886d82676c9de3c57dd8
Oliver Forral <intrepion@gmail.com>
2025-12-07 19:32:03 -0800

    Summary and description

--- main.py
+++ main.py
@@ -241,7 +241,12 @@ async def get_items():
     return ["Portal gun", "Plumbus"]
 
 
-@app.post("/items/", response_model=Item, tags=["items"])
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+)
 async def create_item(item: Item):
     return item
 
