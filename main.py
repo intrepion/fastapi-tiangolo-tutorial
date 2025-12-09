ccef8735462157b02c5a49f3fd17be1c969278b1
Oliver Forral <intrepion@gmail.com>
2025-12-09 04:16:43 -0800

    Update a Hero with HeroUpdate

--- main.py
+++ main.py
@@ -498,6 +498,19 @@ def read_hero(hero_id: int, session: SessionDep):
     return hero
 
 
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
+
+
 @app.post("/images/multiple/")
 async def create_multiple_images(images: list[Image]):
     return images
