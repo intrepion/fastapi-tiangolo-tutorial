35890e3f4283d27ca0506495a612c56217441a4f
Oliver Forral <intrepion@gmail.com>
2025-12-09 04:27:19 -0800

    Dependency Injection

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ from .routers import items, users
 app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
+def get_query(background_tasks: BackgroundTasks, q: str | None = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
+
+
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+
 def write_notification(email: str, message=""):
     with open("log.txt", mode="w") as email_file:
         content = f"notification for {email}: {message}"
@@ -30,6 +42,9 @@ async def root():
 
 
 @app.post("/send-notification/{email}")
-async def send_notification(email: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(write_notification, email, message="some notification")
-    return {"message": "Notification sent in the background"}
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: Annotated[str, Depends(get_query)]
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent"}
