93a46482725d391ed5a8d61c4b3e9a499748e049
Oliver Forral <intrepion@gmail.com>
2025-12-08 20:50:50 -0800

    Read One Hero

--- main.py
+++ main.py
@@ -462,6 +462,14 @@ def create_hero(hero: Hero, session: SessionDep) -> Hero:
     return hero
 
 
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
 @app.post("/images/multiple/")
 async def create_multiple_images(images: list[Image]):
     return images
